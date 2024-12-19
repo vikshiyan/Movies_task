5dbceeb35b50eefa9b485c09c474d9439d026321
Viktoriia Shyian <miss.shiyan@gmail.com>
2024-12-20 00:45:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# My Project Template
+
+## Як використовувати шаблон
+
+1. Натисніть **"Use this template"**.
+2. Створіть новий репозиторій із цього шаблону.
+3. Клонувати репозиторій:
+   ```bash
+   git clone https://github.com/your-username/your-repository-name.git
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+
+    </>
+  );
+}
